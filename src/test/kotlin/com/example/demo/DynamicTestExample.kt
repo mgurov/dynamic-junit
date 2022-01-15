f44cec1483463ff9a6820a1015db83cdc5c16f28
Mykola Gurov <ngurov@gmail.com>
2022-01-16 00:21:35 +0100

    but I guess that's fine

--- src/test/kotlin/com/example/demo/DynamicTestExample.kt
+++ src/test/kotlin/com/example/demo/DynamicTestExample.kt
@@ -2,14 +2,13 @@ package com.example.demo
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DynamicNode
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 
 fun isPalindrome(s: String?): Boolean {
     return s?.reversed() == s
@@ -28,6 +27,11 @@ class TestExample {
         )
     }
 
+    @BeforeEach
+    fun doBeforeEach() {
+        println("before each")
+    }
+
     @ParameterizedTest
     @MethodSource("getTestCases")
     fun `is a palindrome or not - parameterized style`(testCase: Pair<String, Boolean>) {
@@ -36,7 +40,14 @@ class TestExample {
 
     @TestFactory
     fun `is a palindrome or not - dynamic style`(): List<DynamicNode> {
-        return testCases.map { (given, expected) ->
+        println("is a palindrome or not - dynamic style")
+        return listOf(
+            "poop" to true,
+            "poo" to false,
+            "" to true,
+            " a" to false,
+            null to true,
+        ).map { (given, expected) ->
             dynamicTest("$given -> $expected") {
                 assertThat(isPalindrome(given)).isEqualTo(expected)
             }
