910e60ca124ddcb0a91dc182849d8da13e721a8f
Mykola Gurov <ngurov@gmail.com>
2022-01-15 12:28:21 +0100

    okokok

---
+++ src/test/kotlin/com/example/demo/DynamicFuncSpecExample.kt
@@ -0,0 +1,52 @@
+package com.example.demo
+
+import io.kotest.core.config.configuration
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.config.enabledOrReasonIf
+import io.kotest.matchers.string.shouldHaveLength
+import org.assertj.core.api.Assertions.assertThat
+
+class DynamicTests : FunSpec({
+
+    listOf(
+        "sam",
+        "pam",
+        "tima",
+    ).forEach {
+        context("$it should be a three letter name") {
+
+            val givenValue1 = "value1" + it
+
+            context("blah") {
+                assertThat(givenValue1).isEqualTo("blah")
+                test("nested2") {
+                    assertThat(givenValue1).isEqualTo("blah")
+                }
+            }
+
+            test("nested 1") {
+                assertThat(givenValue1).isEqualTo("blah")
+            }
+
+            assertThat(it).hasLineCount(1)
+        }
+    }
+})
+
+class AnotherTest : FunSpec({
+
+    val value1 = "value1"
+    context("context1") {
+        //assertThat(value1).isEqualTo("blah")
+        context("the nested one").config(enabled = value1 != "value1") {
+
+            //assertThat(value1).isEqualTo("blah")
+            test("the nested another one") {
+                assertThat(value1).isEqualTo("blah")
+            }
+            test("the nested another one") {
+                assertThat(value1).isEqualTo("blah")
+            }
+        }
+    }
+})
