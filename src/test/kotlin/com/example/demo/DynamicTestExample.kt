8a7590f63896a11da61eab0ce2eb72fa7cc6389a
Mykola Gurov <ngurov@gmail.com>
2022-01-16 00:19:53 +0100

    fine

--- src/test/kotlin/com/example/demo/DynamicTestExample.kt
+++ src/test/kotlin/com/example/demo/DynamicTestExample.kt
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 fun isPalindrome(s: String?): Boolean {
     return s?.reversed() == s
@@ -14,8 +17,8 @@ fun isPalindrome(s: String?): Boolean {
 
 class TestExample {
 
-    @TestFactory
-    fun `is a palindrome or not - dynamic style`(): List<DynamicNode> {
+    companion object {
+        @JvmStatic
         val testCases = listOf(
             "poop" to true,
             "poo" to false,
@@ -23,7 +26,16 @@ class TestExample {
             " a" to false,
             null to true,
         )
+    }
 
+    @ParameterizedTest
+    @MethodSource("getTestCases")
+    fun `is a palindrome or not - parameterized style`(testCase: Pair<String, Boolean>) {
+        assertThat(isPalindrome(testCase.first)).isEqualTo(testCase.second)
+    }
+
+    @TestFactory
+    fun `is a palindrome or not - dynamic style`(): List<DynamicNode> {
         return testCases.map { (given, expected) ->
             dynamicTest("$given -> $expected") {
                 assertThat(isPalindrome(given)).isEqualTo(expected)
