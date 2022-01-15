27faff9c5e6f973a346636af5c9019b39595ca30
Mykola Gurov <ngurov@gmail.com>
2022-01-16 00:15:24 +0100

    we're happy

--- src/test/kotlin/com/example/demo/DynamicTestExample.kt
+++ src/test/kotlin/com/example/demo/DynamicTestExample.kt
@@ -1,9 +1,42 @@
 package com.example.demo
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+
 fun isPalindrome(s: String?): Boolean {
-    TODO()
+    return s?.reversed() == s
 }
 
-class DynamicTestExample {
+class TestExample {
 
+    @TestFactory
+    fun `is a palindrome or not - dynamic style`(): List<DynamicNode> {
+        val testCases = listOf(
+            "poop" to true,
+            "poo" to false,
+            "" to true,
+            " a" to false,
+            null to true,
+        )
+
+        return testCases.map { (given, expected) ->
+            dynamicTest("$given -> $expected") {
+                assertThat(isPalindrome(given)).isEqualTo(expected)
+            }
+        }
+    }
+
+    @Test
+    fun `is a palindrome or not`() {
+        SoftAssertions.assertSoftly {
+            it.assertThat(isPalindrome("poop")).isTrue()
+            it.assertThat(isPalindrome("poo")).isFalse()
+            it.assertThat(isPalindrome(null)).isFalse()
+        }
+    }
 }
